12d8032e7d8992accdfbdd5562083e7d4b5ed706
Garaisourav12 <garaisourav12@gmail.com>
2024-10-29 21:59:20 +0530

    Implement componentDidUpdate

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const fetchLocalStorage = (key, initialValue) => {
 	const storedValue = localStorage.getItem(key);
@@ -10,7 +10,15 @@ const useLocalStorage = (key, initialValue) => {
 		fetchLocalStorage(key, initialValue)
 	);
 
+	const ref = useRef(true);
+
 	useEffect(() => {
+		if (ref.current) {
+			ref.current = false;
+			return;
+		}
+		console.log(ref.current);
+
 		localStorage.setItem(key, JSON.stringify(state));
 	}, [state]);
 
